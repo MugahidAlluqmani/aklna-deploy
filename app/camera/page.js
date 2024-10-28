4ca5f68a710ab8b6dbc5f1ef8a4f7d25ec6b7ef1
MugahidAlluqmani <mugahidalluqmani@gmail.com>
2024-10-28 04:59:14 +0300

    Fix Camera Page 0.2

--- app/camera/page.js
+++ app/camera/page.js
@@ -65,7 +65,7 @@ export default function CameraPage(){
     return (
         <>
         <div>
-        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Camera Page</span> Scalable AI.</h1>
+        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Camera Page</span> 0.2</h1>
         </div>
         <div style={{ textAlign: 'center' }}>
       {!image ? (

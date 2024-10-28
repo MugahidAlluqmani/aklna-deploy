92cfb36b1ce80a1dc25fbf0079d46bf3ef34c0f1
MugahidAlluqmani <mugahidalluqmani@gmail.com>
2024-10-28 05:06:24 +0300

    Fix Camera Page 0.2.1

--- app/camera/page.js
+++ app/camera/page.js
@@ -65,12 +65,12 @@ export default function CameraPage(){
     return (
         <>
         <div>
-        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Camera Page</span> 0.2</h1>
+        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Camera Page</span> 0.2.1</h1>
         </div>
         <div style={{ textAlign: 'center' }}>
       {!image ? (
         <>
-          <video ref={videoRef} style={{ display: 'inline' }} /> {/* إخفاء الفيديو */}
+          <video height={"50%"} ref={videoRef} style={{ display: 'inline' }} /> {/* إخفاء الفيديو */}
           <button onClick={startCamera}>فتح الكاميرا</button>
           <button onClick={takePicture}>التقاط الصورة</button>
           <button onClick={toggleCamera}>
